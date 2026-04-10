a30b57f7595c613235f71ee4898cf64f62617046
stopwonee <stopw0n@naver.com>
2026-04-10 18:04:07 +0900

    [0409] 스터디 - 대댓글 js

--- everytime/js/detail.js
+++ everytime/js/detail.js
@@ -1,25 +1,34 @@
 const tta_btn = document.querySelector(".ttabong_button");
 const scrap_btn = document.querySelector(".scrap_button");
 
-
 const scrap_btn_text = scrap_btn.querySelector("div");
-
 
 const tta_display_num = document.querySelector(".ttabong_count");
 const scrap_display_num = document.querySelector(".scrap_count");
 
+const rethatguel_btn = document.getElementById("reReply");
+const rethatguel_popup = document.querySelector(".boxForHide");
+
+const thatguel = document.querySelector(".thatguelChang");
+const rethatguel = document.querySelector(".reThatguelChang");
+const thatguel_submit = document.querySelector(".submit_comment_tg");
+const rethatguel_submit = document.querySelector(".submit_comment_rtg")
+
 let tta_count = 0;
 let scr_count = 0;
 let isScrapped = false;
+
+let thatguelContent;
+let reThatguelContent;
 
 
 tta_btn.addEventListener("click", function () {
     tta_count++;
     tta_display_num.textContent = tta_count;
     
-    tta_btn.disabled = true; // 여기부터 아래 세줄 제미나이가... -> 뭔뜻인지?
-    tta_btn.style.opacity = "0.5"; // 뭔 기능인지... 모르겠다
-    tta_btn.style.cursor = "default"; //이게 왜 필요한거임?
+    tta_btn.disabled = true;
+    tta_btn.style.opacity = "0.5";
+    tta_btn.style.cursor = "default";
 });
 
 
@@ -35,3 +44,16 @@ scrap_btn.addEventListener("click", function() {
     }
     scrap_display_num.textContent = scr_count;
 });
+
+rethatguel_btn.addEventListener("click", function() {
+    if (rethatguel_popup.style.display === "flex") {
+        rethatguel_popup.style.display = "none";
+    } else {
+        rethatguel_popup.style.display = "flex";
+    }
+})
+
+rethatguel_submit.addEventListener("click", function() {
+    rethatguel.value = reThatguelContent;
+    alert(reThatguelContent);
+})
